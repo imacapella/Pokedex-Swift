c90f793cd42fa709e87c6eb8ce05e56901ee4b09
imacapella <gurkankaradas42@gmail.com>
2025-03-20 12:21:59 +0300

    Initial Commit

---
+++ Pokedex-Swift/Model/PokeModel.swift
@@ -0,0 +1,19 @@
+//
+//  PokeModel.swift
+//  Pokedex-Swift
+//
+//  Created by Gürkan Karadaş on 20.03.2025.
+//
+
+import Foundation
+
+struct PokeModel{
+    let id: Int
+    let name: String
+    let baseExperience: Int
+    let height: Int
+    let weight: Int
+    let isDefault: Bool
+    let order: Int
+    
+}
---
+++ Pokedex-Swift/Network/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Pokedex-Swift
+//
+//  Created by Gürkan Karadaş on 20.03.2025.
+//
+
+import Foundation
+
+public protocol NetworkManagerProtocol {
+    
+}
+
+final class NetworkManager: NetworkManagerProtocol {
+    
+}
